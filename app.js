75dfe9d8c54c3e0207ad378232c6a8e3529d3894
raposamillar <lisa.raposamillar@gmail.com>
2022-08-23 14:21:48 -0400

    HTML file generated with modularized code

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-  // console.log(profileDataArgs);
+const fs = require('fs');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?  
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = require('./src/page-template.js');
 
-  console.log('=============');
+const profileDataArgs = process.argv.slice(2);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => 
-    console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw new Error (err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
